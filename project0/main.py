78156ee6485e96c7ff74316136f2b3d3cabcaafe
SaisriPotluru <60228240+SaisriPotluru@users.noreply.github.com>
2020-03-03 22:45:37 -0600

    Update main.py

--- project0/main.py
+++ project0/main.py
@@ -6,7 +6,7 @@ import re
 import sqlite3
 import argparse
 import pandas as pd
-
+#to divide the list as list of lists
 def divide_chunks(l, n): 
       
     # looping till length l 
@@ -17,39 +17,39 @@ def divide_chunks(l, n):
 #fetch incidents
 def fetchincidents(url):
 
-    data = urllib.request.urlopen(url).read()
+    data = urllib.request.urlopen(url).read() #the content is stored in the data 
     return data
 
 
 
 #extract incidents
 def extractincidents(data):
-    temp=tempfile.TemporaryFile()
+    temp=tempfile.TemporaryFile() #the data is stored in temp 
     temp.write(data)
     temp.seek(0)
-    pdfReader = PyPDF2.pdf.PdfFileReader(temp)
-    number_of_pages =pdfReader.getNumPages()
+    pdfReader = PyPDF2.pdf.PdfFileReader(temp) #to read the data
+    number_of_pages =pdfReader.getNumPages() #to get the count of total number of pages in pdf
     h1 = []
-    for pn in range(number_of_pages):
+    for pn in range(number_of_pages): #loop for getting all the data in the pdf
         p=pdfReader.getPage(pn)
         pagecontent=p.extractText() 
     #     print(repr(pagecontent))
-        if pn == 0:
+        if pn == 0:   #for the first page
             pagecontent = pagecontent.replace(' \n',' ')
-            pagecontent = pagecontent.split('\n')[5:-3]
+            pagecontent = pagecontent.split('\n')[5:-3] #the headings and the extra strings are deleted
 
 
     #         print(pagecontent)
-        elif pn == (number_of_pages-1):
+        elif pn == (number_of_pages-1): # for the last page 
             pagecontent = pagecontent.replace(' \n',' ')
-            pagecontent = pagecontent.split('\n')[:-2]
+            pagecontent = pagecontent.split('\n')[:-2] #the extra string of date is removed
 
     #         print(pagecontent)
-        else :
+        else :     #for all other pages
             pagecontent = pagecontent.replace(' \n',' ')
-            pagecontent = pagecontent.split('\n')[:-1]
+            pagecontent = pagecontent.split('\n')[:-1] #for each of the page an extra list is formed which is removed
 
-        h = list(divide_chunks(pagecontent, 5)) 
+        h = list(divide_chunks(pagecontent, 5)) #the list is formed as a list of list of 5
     #     print (h)      
         h1.extend(h)
         dfFULLx = pd.DataFrame.from_records(h1,columns=('incident_time','incident_number','incident_location','nature','incident_ori'))
@@ -60,9 +60,9 @@ def extractincidents(data):
 
 #create db
 def createdb():
-    conn = sqlite3.connect('NORMANPOLICE.db')
+    conn = sqlite3.connect('NORMANPOLICE.db') #connecting to the db
     c = conn.cursor()
-    c.executescript('drop table if exists incidents')
+    c.executescript('drop table if exists incidents') #if there is a table incidents exists then its droped prior to creation
     c.execute('''CREATE TABLE incidents (
                 incident_time TEXT,
                 incident_number TEXT,
@@ -74,9 +74,9 @@ def createdb():
 
 #populate db
 def populatedb(db,table):
-    conn = sqlite3.connect(db)
+    conn = sqlite3.connect(db)  #forming the connection
     c = conn.cursor()
-    table.to_sql('incidents', con=conn, index=False, if_exists='replace')
+    table.to_sql('incidents', con=conn, index=False, if_exists='replace')  #to push the data in table
 
     
 
@@ -84,10 +84,10 @@ def populatedb(db,table):
 def status(db):
     conn = sqlite3.connect(db)
     c = conn.cursor()
-    c.execute("SELECT nature, count(*) as count FROM incidents GROUP BY nature order by nature ")
+    c.execute("SELECT nature, count(*) as count FROM incidents GROUP BY nature order by nature ") #query to get the nature with count
     result=c.fetchall()
 #    print(result)
-    result1=['|'.join(str(n) for n in x) for x in result]
+    result1=['|'.join(str(n) for n in x) for x in result]  #replacing comma with '|' operator
 #    print(result1)
     return result1 
 
@@ -95,7 +95,7 @@ def status(db):
 
 def main(url):
     # Download data
-    data=fetchincidents(url)
+    data=fetchincidents(url) #calling the functions
 
     # Extract Data
     incidents = extractincidents(data)
